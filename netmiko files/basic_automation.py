051647845bb5dd25fd23b4f659a80209fc1aecdc
jayspalma <jayshobbyrc@gmail.com>
2020-05-21 20:30:21 +0800

    Remove hard coded username and password instead ask use for the username and password of the device. Also added execptions

--- netmiko files/basic_automation.py
+++ netmiko files/basic_automation.py
@@ -1,22 +1,36 @@
 #Import the ConnectHandler from netmiko library
 from netmiko import ConnectHandler
+#Import getpass to be used on password retrieval
+from getpass import getpass
+#Import necessary modules for password retrieval
+from netmiko.ssh_exception import NetMikoTimeoutException
+from paramiko.ssh_exception import SSHException
+from netmiko.ssh_exception import AuthenticationException
+
+
+
+####DECLARE VARIABLES###
+
+#Ask user for username and password
+print("Please iput user credentials")
+username = input("Username: ")
+password = getpass()
 
 '''These are the devices to be configured.
 For device_type value, it can be found here: https://github.com/ktbyers/netmiko/blob/master/netmiko/ssh_dispatcher.py#L70'''
 R1 = { 
 'device_type': 'cisco_ios', 
 'host': '192.168.1.1', 
-'username': 'cisco', 
-'password': '12345678',
+'username': username, 
+'password': password,
 }
 
 R2 = { 
 'device_type': 'cisco_ios', 
 'host': '192.168.1.2', 
-'username': 'cisco', 
-'password': '12345678',
+'username': username, 
+'password': password,
 }
-
 
 '''Make a dictionary of configuration file and device to be configured.
 Example:
@@ -24,10 +38,31 @@ Example:
 R2 => dictionary of the device stated on first part'''
 devices = {'R1':R1,'R2':R2}
 
+
+
+
+
+
 #Loop through the devices dictionary
 for config,device in devices.items():
+
+	#Handle exceptions
+	try:
+		net_connect = ConnectHandler(**device)
+	except(AuthenticationException):
+		print("Wrong username or password on " + device['host'] )
+		continue
+	except(SSHException):
+		print("Failed to connect to " + device['host'])
+		continue
+	except (NetMikoTimeoutException):
+		print ('Timeout to device: ' + device['host'])
+		continue
+	except Exception as unknown_error:
+		print("Some other error: " + str(unknown_error))
+		continue
+
 	print("Connecting to "  + device['host'])
-	net_connect = ConnectHandler(**device)
 	print("Configuring " + device['host'] + " with configuration file " + config  )
 
 	file = open(config)
